c3efc2627a116ae47065474aa1c59e747f35e14f
stevet <stevetonneau@gotmail.fr>
2019-10-04 12:24:53 +0200

    fixed split

--- python/test/variable/test_var.py
+++ python/test/variable/test_var.py
@@ -12,12 +12,12 @@ waypointsA = array([[1.,2.,3.],[4.,5.,6.],[7.,8.,9.],[1.,2.,3.],[4.,5.,6.],[7.,8
 waypointsb = array([[1.,2.,3.],[1.,2.,3.]]).transpose()
 
 #testing bezier curve
-a = bezierVar(waypointsA, waypointsb, 3.)
+a = bezierVar(waypointsA, waypointsb,0.,3.)
 
 subBeziers = a.split(array([[0.2,0.4]]).transpose())
 assert(subBeziers.size == 3)
 assert(subBeziers.at(0).max() - 0.2 <= __EPS)
-assert(subBeziers.at(1).max() - 0.2 <= __EPS)
-assert(subBeziers.at(2).max() - 2.6 <= __EPS)
+assert(subBeziers.at(1).max() - subBeziers.at(1).min() - 0.2 <= __EPS)
+assert(subBeziers.at(2).max() - subBeziers.at(2).min() - 2.6 <= __EPS)
 
 
